63eb8f835c299761a0b8bac75924eb51fdbb25a1
s-k-22 <swar.khatale@gmail.com>
2023-11-09 10:50:26 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ExpenseItem = () => {
+  return (
+    <div>
+      ExpenseItem
+      <div>Food Rs 10 </div>
+      <div>Petrol Rs 100 </div>
+      <div>Movies Rs 200</div>
+    </div>
+  );
+}
+
+export default ExpenseItem
