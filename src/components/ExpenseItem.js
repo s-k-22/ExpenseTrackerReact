92c8907ae4f628b22d49dc3fa32f9fa6eebfbb35
s-k-22 <swar.khatale@gmail.com>
2023-11-10 14:58:31 +0530

    expenseItem.css is added and using js in jsx code

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,20 @@
-import React from 'react'
+import "./ExpenseItem.css";
 
-const ExpenseItem = () => {
+function ExpenseItem() {
+  const expenseDate = new Date(2023, 11, 10);
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+  const locationOfExpenditure = "India";
   return (
-    <div>
-      ExpenseItem
-      <div>Food Rs 10 </div>
-      <div>Petrol Rs 100 </div>
-      <div>Movies Rs 200</div>
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div>{locationOfExpenditure}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
     </div>
   );
 }
 
-export default ExpenseItem
+export default ExpenseItem;
