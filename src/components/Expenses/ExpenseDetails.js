4d7b9348fec0aa0cbb01450e5d25c5a8bbd7c261
s-k-22 <swar.khatale@gmail.com>
2023-12-27 14:38:36 +0530

    update amount button is added

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 
 export const ExpenseDetails = (props) => {
+  const [title, setTitle] = useState(props.title);
+  const [amount, setamount] = useState(props.amount);
+  // console.log("title ", title); //latest value of title
   const clickHandler = () => {
-    console.log("clicked");
+    setTitle("updated");
+    console.log(title);
   };
+
   const deleteExpense = () => {
     const expense = document.querySelector(".expense-item");
-    // console.log(expense);
     expense.remove();
   };
+
+  const updateAmount = () => {
+    setamount(100);
+  };
+
   return (
     <div>
-      <div>{props.locationOfExpenditure}</div>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
+        <h2> {props.locationOfExpenditure}</h2>
+      </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
       <button onClick={clickHandler}>change title</button>
       <button onClick={deleteExpense}>Delete Expense</button>
+      <button onClick={updateAmount}>Update Amount</button>
     </div>
   );
 };
