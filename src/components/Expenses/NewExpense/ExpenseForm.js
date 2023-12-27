3500cfbc29503cc378706f9ba7ce4840f307c296
s-k-22 <swar.khatale@gmail.com>
2023-12-27 19:03:55 +0530

    clear input fields - 2 way binding

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -49,6 +49,9 @@ const ExpenseForm = () => {
     };
 
     console.log(expenseData);
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
   };
 
   return (
@@ -56,15 +59,23 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" onChange={dateChangeHandler} />
+          <input type="date" value={enteredDate} onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
