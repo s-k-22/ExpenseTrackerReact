adebde1a9003c737625202eb0ca4de2f006ec055
s-k-22 <swar.khatale@gmail.com>
2023-12-27 17:02:30 +0530

    state in component for all input changes

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
   const titleChangeHandler = (e) => {
-    console.log(e.target.value);
+    setEnteredTitle(e.target.value);
   };
 
   const amountChangeHandler = (e) => {
-    console.log(e.target.value);
+    setEnteredAmount(e.target.value);
   };
 
   const dateChangeHandler = (e) => {
-    console.log(e.target.value);
+    setEnteredDate(e.target.value);
   };
 
   return (
@@ -20,8 +24,12 @@ const ExpenseForm = () => {
         <div className="new-expense__control">
           <label>Title</label>
           <input type="text" onChange={titleChangeHandler} />
+        </div>
+        <div className="new-expense__control">
           <label>Amount</label>
           <input type="number" onChange={amountChangeHandler} />
+        </div>
+        <div className="new-expense__control">
           <label>Date</label>
           <input type="date" onChange={dateChangeHandler} />
         </div>
